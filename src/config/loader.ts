8be6302c0fe4f104cf235a412ff84d1a37734654
Tarik02 <Taras.Fomin@gmail.com>
2025-01-27 17:33:12 +0200

    add debug logging for loadConfig

--- src/config/loader.ts
+++ src/config/loader.ts
@@ -6,9 +6,11 @@ import { ConfigNotFoundError } from '../errors';
 
 export const loadConfig = async (cwd: string) => {
     for (const file of ['run-linters.config.js', 'run-linters.config.ts']) {
+        console.log(`Checking for config file ${file}`);
         try {
             await fs.stat(path.join(cwd, file));
         } catch (error) {
+            console.log(error);
             if ((error as { code?: string }).code !== 'ENOENT') {
                 throw error;
             }
@@ -24,5 +26,6 @@ export const loadConfig = async (cwd: string) => {
         return config;
     }
 
+    console.log('Config file not found');
     throw new ConfigNotFoundError();
 };
